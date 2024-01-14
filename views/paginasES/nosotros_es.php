aadb3c3216d7f09eee60eb41c13c0c88044dea37
Jose <jignacio20072@gmail.com>
2024-01-14 20:10:54 -0300

    nosotros

--- views/paginasES/nosotros_es.php
+++ views/paginasES/nosotros_es.php
@@ -1,3 +1,46 @@
-<div>
-    <h1><?php echo $titulo; ?></h1>
-</div>
+<main class="nosotros">
+    <div class="caja_nosotros">
+        <h1>Nosotros</h1>
+        <p>Expertos en Inversiones y Financiamiento</p>
+        <div class="caja_boton_consulta">
+            <a class="boton_consulta" href="#">AGENDA UNA CONSULTA</a>
+        </div>
+    </div>
+    <div class="midseccion">
+        <div class="caja_titulo">
+            <h2>Bienvenido a SCRIVENA</h2>
+            <p>Nos enorgullece ser líderes en la gestión de inversiones en el sector minero, bursátil y bienes raíces, así como en la administración de financiamientos para proyectos en general.</p>
+        </div>
+        <div class="nosotros1">
+            <div>
+                <h3>Nuestra Experiencia</h3>
+                <p>Nuestro equipo de expertos, con más de 30 años de experiencia, ofrece soluciones financieras innovadoras y rentables, garantizando el crecimiento y la maximización de las inversiones de nuestros clientes.</p>
+            </div>
+            <div>
+                <h3>Nuestro Enfoque</h3>
+                <p>Colaboramos estrechamente con aliados estratégicos que comparten nuestra visión, enfocándonos en entender y adaptar nuestras soluciones financieras a las necesidades específicas de cada cliente, para asegurar resultados excepcionales en nuestros proyectos.</p>
+            </div>
+            <div>
+                <h3>Presencia Global</h3>
+                <p>Nuestra presencia global en países como Estados Unidos, México, Colombia, Costa Rica, Panamá, Emiratos Árabes y Suiza nos ha permitido crear una red sólida para acceder a oportunidades de inversión únicas y diversificadas, enfocándonos en generar valor sostenible y resultados a largo plazo para nuestros clientes.</p>
+            </div>
+        </div>
+    </div>
+    <div class="nosotros2">
+        <h3>SCRIVENA: Tu aliado en inversiones para prosperidad financiera. Explora y crece con nosotros.</h3>
+    </div>
+    <div class="nosotros3">
+        <div class="valores">
+            <h3>Nuestros valores</h3>
+            <p>En SCRIVENA, creemos en la transparencia, la integridad y la ética en todas nuestras operaciones. Estos principios nos guían en todas nuestras decisiones y estrategias, asegurando que mantenemos la confianza y el respeto de nuestros clientes y socios.</p>
+        </div>
+        <div class="objetivos">
+            <h3>Nuestro objetivo</h3>
+            <p>SCRIVENA se enfoca en ayudar a sus clientes a alcanzar metas financieras, especializándose en inversiones en minería, bolsa, bienes raíces y remates inmobiliarios, y ofreciendo financiamiento personalizado para proyectos variados. Nos comprometemos a la excelencia y la innovación para lograr resultados excepcionales.</p>
+        </div>
+    </div>
+    <div class="nosotros4">
+        <h3>Con la confianza de cientos de clientes satisfechos</h3>
+        <p>¡Contáctanos hoy mismo y descubre cómo podemos hacer realidad tus proyectos de inversión! Estamos listos para escuchar tus metas y necesidades financieras y para explorar cómo podemos ayudarte a alcanzarlas.</p>
+    </div>
+</main>
