1ad2542b16687174e666461051120abd355e8ddf
Jose <jignacio20072@gmail.com>
2024-01-15 02:30:45 -0300

    portafolio

---
+++ views/paginasES/portafolio_es.php
@@ -0,0 +1,53 @@
+<main class="portafolio">
+    <div class="caja_portafolio">
+        <h1>Portafolio</h1>
+        <p>Rendimientos Sólidos - Conoce Nuestro Portafolio</p>
+        <div class="caja_boton_consulta">
+            <a class="boton_consulta" href="#">AGENDA UNA CONSULTA</a>
+        </div>
+    </div>
+    <div class="midseccion">
+        <div class="portafolio_padre">
+            <div class="portafolio1">
+                <h3>Inversión en Minería (Oro, Plata, Minerales Estratégicos)</h3>
+                <p>La inversión en minería, incluyendo oro, plata y minerales estratégicos, es clave para diversificar portafolios, ofreciendo altos retornos a largo plazo y apoyando el desarrollo económico. Esta inversión permite a los inversionistas participar en la expansión de minas y en avances tecnológicos, contribuyendo a la sostenibilidad de la industria.</p>
+                <div class="caja_imagen_portafolio">
+                    <picture>
+                        <source srcset="build/img/oro.avif" type="image/avif" loading="lazy">
+                        <source srcset="build/img/oro.webp" type="image/webp" loading="lazy">
+                        <img class="img-carrusel" loading="lazy" src="build/img/oro.png" alt="Imagen Carrousel 1">
+                    </picture>
+                </div>
+            </div>
+            <div>
+                <hr class="divisor">
+            </div>
+            <div class="portafolio2">
+                <h3>Desarrollos Inmobiliarios</h3>
+                <p>
+La inversión en desarrollos inmobiliarios, abarcando propiedades residenciales, comerciales, industriales y recreativas, puede generar retornos financieros sólidos y beneficios tangibles. Ofrece ingresos estables por alquileres y ventas, además de potenciales ganancias de capital por apreciación del valor. Además, actúa como una cobertura contra la inflación, ya que el valor de la propiedad suele incrementarse con el tiempo.</p>
+                <div class="caja_imagen_portafolio">
+                    <picture>
+                        <source srcset="build/img/desarrollo.avif" type="image/avif" loading="lazy">
+                        <source srcset="build/img/desarrollo.webp" type="image/webp" loading="lazy">
+                        <img class="img-carrusel" loading="lazy" src="build/img/desarrollo.png" alt="Imagen Carrousel 1">
+                    </picture>
+                </div>
+            </div>
+            <div>
+                <hr class="divisor">
+            </div>
+            <div class="portafolio3">
+                <h3>Fondos de Inversión de Estados Unidos</h3>
+                <p>Invertir en el fondo SCRIVENA es un movimiento estratégico para diversificar portafolios. Respaldado por Cerberus Capital Management y Ares Capital Corporation en EE. UU., ofrece una plataforma de inversión segura. Nuestra misión es tu prosperidad financiera, y gracias a estas alianzas, proporcionamos acceso exclusivo a portafolios de inversión normalmente no disponibles para el público, con una gestión y rendimiento excelentes.</p>
+                <div class="caja_imagen_portafolio">
+                    <picture>
+                        <source srcset="build/img/eeuu.avif" type="image/avif" loading="lazy">
+                        <source srcset="build/img/eeuu.webp" type="image/webp" loading="lazy">
+                        <img class="img-carrusel" loading="lazy" src="build/img/eeuu.png" alt="Imagen Carrousel 1">
+                    </picture>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
