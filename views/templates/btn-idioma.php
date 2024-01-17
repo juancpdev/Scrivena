e562071be2e094712052b72c9cc0c3f79dcd594d
Juan Cruz <juan_pineda_12@hotmail.es>
2024-01-17 02:29:45 -0300

    btn-idioma

--- views/templates/btn-idioma.php
+++ views/templates/btn-idioma.php
@@ -1,12 +1,12 @@
 <?php if ($isHomePage): ?>
     <div class="language-toggle">
-        <div class="language-option <?php echo $langParam === "es" ? "es" : "none" ?>">
+        <div class="language-option <?php echo $langParam === "es" ? "es" : "desactivado" ?>">
             ES
         </div>
         <div class="toggle-button">
             <div class="circle"></div>
         </div>
-        <div class="language-option <?php echo $langParam === "en" ? "en" : "none" ?>">
+        <div class="language-option <?php echo $langParam === "en" ? "en" : "desactivado" ?>">
             EN
         </div>
     </div>
