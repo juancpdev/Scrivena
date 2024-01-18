7087cd1544cb947c32d7e3a8c04473e91e0c53e0
Jose <jignacio20072@gmail.com>
2024-01-18 10:43:30 -0300

    tituloyseo

--- views/layout.php
+++ views/layout.php
@@ -11,8 +11,38 @@
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css" integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ==" crossorigin="" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="/build/css/app.css?v=1.2">
+    <!-- HTML Meta Tags -->
+    <title>Scrivena - Líder en Inversiones</title>
+    <meta name="description" content="Invierte en el futuro con nuestro fondo de inversión líder, ofreciendo estrategias personalizadas y rendimientos sostenibles.">
+
+    <!-- Facebook Meta Tags -->
+    <meta property="og:url" content="https://scrivena.com/">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Scrivena - Líder en Inversiones">
+    <meta property="og:description" content="Invierte en el futuro con nuestro fondo de inversión líder, ofreciendo estrategias personalizadas y rendimientos sostenibles.">
+    <meta property="og:image" content="https://opengraph.b-cdn.net/production/documents/8ae9a36d-be4d-4d4f-8a26-aaa8bbcb386c.png?token=I74xyBWSOW3jtZ-rwojdXXcBjz-FKf7pVhMVxDx9fQM&height=630&width=1200&expires=33241576709">
+
+    <!-- Twitter Meta Tags -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta property="twitter:domain" content="scrivena.com">
+    <meta property="twitter:url" content="https://scrivena.com/">
+    <meta name="twitter:title" content="Scrivena - Líder en Inversiones">
+    <meta name="twitter:description" content="Invierte en el futuro con nuestro fondo de inversión líder, ofreciendo estrategias personalizadas y rendimientos sostenibles.">
+    <meta name="twitter:image" content="https://opengraph.b-cdn.net/production/documents/8ae9a36d-be4d-4d4f-8a26-aaa8bbcb386c.png?token=I74xyBWSOW3jtZ-rwojdXXcBjz-FKf7pVhMVxDx9fQM&height=630&width=1200&expires=33241576709">
+
+<!-- Meta Tags Generated via https://www.opengraph.xyz -->
+
     <script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js" integrity="sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ==" crossorigin="" defer></script>
     <script src="https://kit.fontawesome.com/477cb6ab4e.js" crossorigin="anonymous"></script>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-DJ84FH707W"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-DJ84FH707W');
+    </script>
 </head>
 <body>
     <?php 
