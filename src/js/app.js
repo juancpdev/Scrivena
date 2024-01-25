bc7e5fc4a07ca79ff4eea2ab8eb20632d4232cce
Juan Cruz <juan_pineda_12@hotmail.es>
2024-01-24 23:13:01 -0300

    v=1.6

--- src/js/app.js
+++ src/js/app.js
@@ -180,3 +180,15 @@ function alertaFormulario() {
     }, 4000);
   }
 }
+
+// ChatBot
+(function(d, w, c) {
+  w.BrevoConversationsID = '65aad6e5e1f72d4bfd439584';
+  w[c] = w[c] || function() {
+      (w[c].q = w[c].q || []).push(arguments);
+  };
+  var s = d.createElement('script');
+  s.async = true;
+  s.src = 'https://conversations-widget.brevo.com/brevo-conversations.js';
+  if (d.head) d.head.appendChild(s);
+})(document, window, 'BrevoConversations');
--- views/layout.php
+++ views/layout.php
@@ -1,17 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<script>
-  (function(d, w, c) {
-      w.BrevoConversationsID = '65aad6e5e1f72d4bfd439584';
-      w[c] = w[c] || function() {
-          (w[c].q = w[c].q || []).push(arguments);
-      };
-      var s = d.createElement('script');
-      s.async = true;
-      s.src = 'https://conversations-widget.brevo.com/brevo-conversations.js';
-      if (d.head) d.head.appendChild(s);
-  })(document, window, 'BrevoConversations');
-</script>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -22,7 +11,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css" integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ==" crossorigin="" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="/build/css/app.css?v=1.5">
+    <link rel="stylesheet" href="/build/css/app.css?v=1.6">
     <!-- HTML Meta Tags -->
     <title>Scrivena - Líder en Inversiones</title>
     <meta name="description" content="Invierte en el futuro con nuestro fondo de inversión líder, ofreciendo estrategias personalizadas y rendimientos sostenibles.">
@@ -74,6 +63,6 @@
         }
         
     ?>
-    <script src="/build/js/bundle.min.js?v=1.5" defer></script>
+    <script src="/build/js/bundle.min.js?v=1.6" defer></script>
 </body>
 </html>
