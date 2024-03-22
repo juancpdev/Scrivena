7ec1d2436017bb0dd38348e935d70feb41d1d1b4
Juan Cruz <juan_pineda_12@hotmail.es>
2024-03-22 12:34:55 -0300

    2.1

--- views/layout.php
+++ views/layout.php
@@ -24,7 +24,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css" integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ==" crossorigin="" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="/build/css/app.css?v=2.0">
+    <link rel="stylesheet" href="/build/css/app.css?v=2.1">
     <!-- HTML Meta Tags -->
     <title>Scrivena - Líder en Inversiones</title>
     <meta name="description" content="Invierte en el futuro con nuestro fondo de inversión líder, ofreciendo estrategias personalizadas y rendimientos sostenibles.">
@@ -80,7 +80,7 @@
     }
 
     ?>
-    <script src="/build/js/bundle.js?v=2.0" defer></script>
+    <script src="/build/js/bundle.js?v=2.1" defer></script>
     <script src='//cdn.jsdelivr.net/npm/sweetalert2@10'></script>
 </body>
 
